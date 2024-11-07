cec72f7be598340578e537032178c56559bbf177
Nikita Plotnikov <plotnikov.nikitos2014@mail.ru>
2024-11-07 23:14:34 +0300

    Changed task, Задание 1 и 2 выполнено.

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d, x1, x2;
+  d = Math.pow(b, 2) - 4 * a * c;
+  if (d < 0) {
+    return arr;
+  } else if (d === 0) {
+    x1 = -b / (2 * a);
+    arr.push(x1);
+  } else {
+    x1 = (-b + Math.sqrt(d)) / (2 * a);
+    x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1, x2);
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthlyRate = (percent / 100) / 12;
+  let principal = amount - contribution;
+  let monthPayment = principal * (monthlyRate + (monthlyRate / (Math.pow(1 + monthlyRate, countMonths) - 1)));
+  let totalPayment = monthPayment * countMonths;
+  return parseFloat(totalPayment.toFixed(2));
 }
