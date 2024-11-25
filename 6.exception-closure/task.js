fb0ff77a5a2fcd0968117395091379a07af529a0
Nikita Plotnikov <plotnikov.nikitos2014@mail.ru>
2024-11-25 13:00:06 +0300

    add homework exception

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,58 @@
-﻿
+﻿function parseCount(value) {
+  const result = Number.parseFloat(value);
+  if (isNaN(result)) {
+    throw new Error ("Невалидное значение");
+  }
+  return result;
+}
+
+function validateCount(value) {
+  try {
+    return parseCount(value);
+  } catch (error) {
+    return error;
+  }
+}
+class Triangle {
+  constructor(a, b, c) {
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  get perimeter() {
+    return this.a + this.b + this.c;
+  }
+
+  get area() {
+    const p = this.perimeter / 2;
+    return +Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(
+      3
+    );
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch {
+    return {
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";
+      },
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      },
+    };
+  }
+}
+const triangle = new Triangle(3, 4, 5);
+console.log(triangle.perimeter);
+console.log(triangle.area);
+
+const invalidTriangle = getTriangle(1, 1, 3);
+console.log(invalidTriangle.perimeter); //
+console.log(invalidTriangle.area);
